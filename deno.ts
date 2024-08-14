3b8f9f7a70dbe0fe21b69b3c5b88bcf844f233fe
飞扬羊sheep <138973915+flysheep0208@users.noreply.github.com>
2024-08-14 20:36:51 +0800

    Update deno.ts again

--- deno.ts
+++ deno.ts
@@ -1,10 +1,8 @@
-const OPENAI_API_HOST = "api.openai.com";
-const OPENAI_API_PATH = "/v1";
+const OPENAI_API_HOST = "api.openai.com/v1";
 
 Deno.serve(async (request) => {
   const url = new URL(request.url);
   url.host = OPENAI_API_HOST;
-  url.pathname = OPENAI_API_PATH;
 
   const newRequest = new Request(url.toString(), {
     headers: request.headers,
